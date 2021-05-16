26d0463167bee6f79245bf902f7028a9899e97b9
Anurag <abaral@extech-ideas.com>
2021-05-16 19:27:51 -0400

    Init Commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy{
+
+    constructor(private productService: ProductService) {
+
+    }
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    filteredProducts: IProduct[] = [];
+    products: IProduct[] = [];
+    errorMessage: string = '';
+    sub!: Subscription;
+
+    private _listFilter: string = '';
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value: string) {
+      this._listFilter = value;
+      this.filteredProducts = this.performFilter(value);
+    }
+
+      performFilter(filterBy: string): IProduct[]{
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy));
+      }
+
+      toggleImage(): void {
+          this.showImage = !this.showImage;
+      }
+
+      onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+      }
+
+      ngOnInit(): void {
+        this.sub = this.productService.getProducts().subscribe({
+          next: products => {
+            this.products = products;
+            this.filteredProducts = this.products;
+          },
+          error: err => this.errorMessage = err
+        });
+      }
+
+      // tslint:disable-next-line:typedef
+      ngOnDestroy() {
+        this.sub.unsubscribe();
+      }
+}
